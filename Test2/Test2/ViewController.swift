ed9848c20805c933c16dce61be5c39928df2aa20
Joe Lucero <jlucerond@gmail.com>
2018-05-14 09:57:10 -0600

    XCode project created

---
+++ Test2/Test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Joe Lucero on 5/14/18.
+//  Copyright © 2018 Joe Lucero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
